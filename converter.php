dbb772ae9bfafac47f1758998002e5f6fc0e2e51
CastenettoA <castenetto.andrea@gmail.com>
2016-03-17 13:50:15 +0100

    First trivial code

---
+++ converter.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * Created by Andrea Castenetto
+ * the program is't ready to use, please wait some days
+ */
+
+    $file_array = array();
+    $n = 0;
+
+    $the_file = file('text.html');
+
+    //assign each line to the array $file_array
+    foreach($the_file as $line_num => $line) {
+       $file_array[$n] = htmlspecialchars($line);
+       $n++;
+    }
+
+    $n = 0;
+    foreach($file_array as $linea){
+        echo $linea .'<br>';
+    }
+
+?>
